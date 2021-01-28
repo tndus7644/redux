c45922edfb13785f56c8defa6d6527a14917eb63
suyeon <tndus7644@gmail.com>
2021-01-28 15:24:58 +0900

    sidebar redux

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = () => {
+
+    return(
+        <Container>
+            Home
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+
+export default Home;
